3b6d96081efa3120c0540376e4ecf7f1ba485be7
coronelee <79922854+coronelee@users.noreply.github.com>
2023-12-18 17:54:03 +0300

    fix

--- src/components/ButtonBarHeader.jsx
+++ src/components/ButtonBarHeader.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 export default function ButtonBarHeader() {
     return (
         <>
-            <a href='#skills' onClick={() => clickMenuHam()}>
+            <a href='skills' onClick={() => clickMenuHam()}>
                 Знания
             </a>
-            <a href='#works' onClick={() => clickMenuHam()}>
+            <a href='works' onClick={() => clickMenuHam()}>
                 Работы
             </a>
-            <a href='#hireme' onClick={() => clickMenuHam()}>
+            <a href='hireme' onClick={() => clickMenuHam()}>
                 Связь со мной
             </a>
         </>
